afc71f8d269a602d10a67580860bf8fe4170d69e
sriluballa <SriluNBalla@Gmail.com>
2025-05-10 18:54:19 -0400

    adding all files

---
+++ component/footer.js
@@ -0,0 +1,42 @@
+class PotFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer class="footer">
+        <link rel="stylesheet" href="/css/footer.css"/>
+        <div class="footer__container">
+          <div class="footer__logo">
+            <a href="/">Product Owner in Test™</a>
+          </div>
+
+          <ul class="footer__menu">
+            <li class="footer__item">
+            <a href="/pages/about.html" class="footer_link">About</a>
+            </li>
+            <li class="footer__item">
+            <a href="/pages/contact.html" class="footer_link">Contact</a>
+            </li>
+            <li class="footer__item">
+            <a href="/pages/privacy.html" class="footer_link">Privacy</a>
+            </li>
+            </ul>
+          </div>
+          <div class="footer__credit">
+            <p>&copy; 2025 Srilu Balla. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+    `;
+
+    // Move script logic into here so it runs
+    const mobileMenu = this.querySelector('#mobile-menu');
+    const navbarMenu = this.querySelector('.navbar__menu');
+
+    mobileMenu?.addEventListener('click', () => {
+      navbarMenu.classList.toggle('active');
+      mobileMenu.classList.toggle('active');
+    });
+  }
+}
+
+// Register the custom element
+customElements.define('pot-foot', PotFooter);
---
+++ component/homeHero.js
@@ -0,0 +1,23 @@
+class HomeHero extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <link rel="stylesheet" href="css/main.css" />
+        <section class="hero">
+            <div class="hero-content">
+                <h2>Welcome to the World of Product Owner in Test™</h2>
+                <p>Empowering people and teams to use Software exploration to comprehend software products end to end through training, certification, mentoring, consulting, and ongoing community experiences.</p>
+                    <div class="hero-buttons">
+                        <a href="#" class="button">Explore Now</a>
+                        <a href="#" class="button">Watch Video</a>
+                    </div>
+            </div>
+    
+            <div class="hero-image">
+                <img src="images/happypots.gif" alt="Curious Testers">
+            </div>
+</section>
+      `
+    }
+};
+
+customElements.define('home-hero', HomeHero);
