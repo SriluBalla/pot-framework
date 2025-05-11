8659330e4bf4fda5f28ad700ddf17f5b4c6952b8
sriluballa <SriluNBalla@Gmail.com>
2025-05-10 21:36:29 -0400

    removed base script

--- scripts/base.js
+++
@@ -1,5 +0,0 @@
-// scripts/base.js
-
-const isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1' || location.protocol === 'file:';
-const base = isLocal ? './' : '/pot-framework/';
-document.write(`<base href="${base}">`);
